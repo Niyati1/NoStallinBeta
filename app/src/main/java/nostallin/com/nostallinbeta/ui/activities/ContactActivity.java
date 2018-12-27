dcab25d689634bccf114f91f5d55bc38bca46a76
Magdiel Lorenzo <Leeran7742@gmail.com>
2018-12-26 20:30:15 -0500

    Moved feedback logic into ContactActivity

--- app/src/main/java/nostallin/com/nostallinbeta/ui/activities/ContactActivity.java
+++ app/src/main/java/nostallin/com/nostallinbeta/ui/activities/ContactActivity.java
@@ -1,8 +1,15 @@
 package nostallin.com.nostallinbeta.ui.activities;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import nostallin.com.nostallinbeta.R;
 
@@ -16,6 +23,15 @@ public class ContactActivity extends AppCompatActivity {
         setTitle(R.string.Contact);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Button feedbackButton = findViewById(R.id.activity_contact_sendFeedback);
+
+        feedbackButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendFeedbackEmail();
+            }
+        });
     }
 
     @Override
@@ -28,5 +44,22 @@ public class ContactActivity extends AppCompatActivity {
     public void onBackPressed() {
         super.onBackPressed();
         finish();
+    }
+
+    private void sendFeedbackEmail() {
+        String[] destEmails = {"noStallinBathroomApp@gmail.com"};
+
+        Intent emailIntent = new Intent(Intent.ACTION_SENDTO);
+
+        emailIntent.setData(Uri.parse("mailto:"));
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, destEmails);
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Feedback");
+
+        if (emailIntent.resolveActivity(getPackageManager()) != null) {
+            startActivity(emailIntent);
+        } else {
+            Log.e(ContactActivity.class.getCanonicalName(), "Could not find activity to handle intent");
+            Toast.makeText(this, R.string.PlaceInfo_Error, Toast.LENGTH_LONG).show();
+        }
     }
 }
